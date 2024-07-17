38ce5058c7de2c764d975b76f92442220a354523
quinm0 <quin@moran1.com>
2024-07-17 16:30:19 -0400

    add chromuim

--- common/guiPrograms.nix
+++ common/guiPrograms.nix
@@ -5,6 +5,7 @@
     alacritty # Terminal app
     vscode # Visual studio code
     signal-desktop # Signal messaging app
+    chromium # Chrome
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
