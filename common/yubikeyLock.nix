19c0622984998b744f0bbe013f2c26202c33c3e6
quinm0 <quin@moran1.com>
2024-07-19 13:32:56 -0400

    hell yeah

--- common/yubikeyLock.nix
+++ common/yubikeyLock.nix
@@ -1,12 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  u2f_keys_content = ''
+    qmoran:ca/RlE2uQqswjfcjFQZx8LWT7W6lXr9W6pcx3YOSO8EsJnTQRO7B+gvYMQG/dlcwj4ea5UuPC3slrMQLG4haHQ==,1N3tbJ2eaDS9A2QCkjxuxctEhIlpf0vAx9SSBMhJoDKKAahk1mUAbT9xv9MA3ipYHBhrLrHU/M7WS3iK9fDUoA==,es256,+presence:xG3YAZmdHPNRrJeT+8mZ/IUwEHe83tzbbAtTDyjufVDGWKcXtv62U+uEj7VhoPL12mGwnNM9zRHet4Y0px/GkQ==,Bj/2u0NJHLwrCQTQwgYefkXVKowPrkin8EEyjDlfUjvBiA1QGtcyRHXSBSJkthKurc9n7s04/za2cpBKQow8LA==,es256,+presence
+  '';
+in
 {
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
+  security.pam = {
+    u2f.authFile = "/etc/Yubico/u2f_keys";
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
 
-    login.unixAuth = false;
-    sudo.unixAuth = false;
+      login.unixAuth = false;
+      sudo.unixAuth = false;
+
+    };
+  };
+
+  environment.etc."Yubico/u2f_keys" = {
+    text = u2f_keys_content;
+    user = "root";
+    group = "root";
+    mode = "0600";
   };
 
   services.udev.extraRules = ''
