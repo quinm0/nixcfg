890f858f3daf4ddd5997d564f86ca9991c0915c8
quinm0 <quin@moran1.com>
2024-07-30 15:20:31 -0400

    Generation 60

--- common/alias.nix
+++ common/alias.nix
@@ -5,5 +5,6 @@
     ecfg = "sudo nano /etc/nixos/quin.nix && sudo nixos-rebuild switch";
     up = "docker compose up -d";
     down = "docker compose down";
+    nixos-config = "/etc/nixos/updateConfig.sh";
   };
 }
