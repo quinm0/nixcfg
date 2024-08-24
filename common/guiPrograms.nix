2545682a57232875a304b9cf64e79f444b5c52de
quinm0 <quin@moran1.com>
2024-08-24 12:11:57 -0400

    that's amazong

--- common/guiPrograms.nix
+++ common/guiPrograms.nix
@@ -1,16 +1,20 @@
 { config, pkgs, ... }:
 
 { 
-  environment.systemPackages = with pkgs; [
-    alacritty # Terminal app
-    vscode # Visual studio code
-    signal-desktop # Signal messaging app
-    spotify
-    prusa-slicer
-    rpi-imager
-    gparted
-    google-chrome
-  ];
+  environment.systemPackages = with pkgs; 
+    let 
+      zen-browser = builtins.getFlake("github:MarceColl/zen-browser-flake");
+    in [
+      alacritty # Terminal app
+      vscode # Visual studio code
+      signal-desktop # Signal messaging app
+      spotify
+      prusa-slicer
+      rpi-imager
+      gparted
+      google-chrome
+      zen-browser.packages."${system}".default
+    ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
