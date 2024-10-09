e01677a809e1f9e2bef54928c09a604a58372dd2
quinm0 <quin@moran1.com>
2024-10-09 13:50:32 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if [ -d "/etc/nixos" ]; then
+  echo "Repository already exists. Skipping cloning."
+else
+  git clone git@github.com:quinm0/nixcfg.git /etc/nixos
+fi
+
+read -p "Enter the machine hostname: " hostname
+
+if [ -f "/etc/nixos/hosts/$hostname.nix" ]; then
+  echo "Host configuration file already exists. Skipping creation."
+else
+  cp /etc/nixos/hosts/template.nix.template "/etc/nixos/hosts/$hostname.nix"
+  sed -i "s/\$HOSTNAME/$hostname/g" "/etc/nixos/hosts/$hostname.nix"
+fi
