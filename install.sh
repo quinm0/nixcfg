a7221bd807b15b7bf55f9ddcfc8a2c31a63d6b81
quinm0 <quin@moran1.com>
2024-10-09 16:34:40 -0400

    update again

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
 
-if [ -d "/etc/nixos" ]; then
-  if [ -d "/etc/nixos/.git" ]; then
-    current_remote=$(git -C /etc/nixos remote get-url origin)
-    expected_remote="git@github.com:quinm0/nixcfg.git"
-    if [ "$current_remote" == "$expected_remote" ]; then
-      echo "Repository already exists and matches the expected remote. Skipping cloning."
-    else
-      echo "Repository already exists but does not match the expected remote. Please update the remote URL."
-    fi
-  else
-    echo "Directory /etc/nixos exists but is not a git repository. Please remove or rename the directory."
-  fi
+if [ -d "/etc/nixos/.git" ]; then
+  echo "Repository already exists. Skipping cloning."
 else
   git clone git@github.com:quinm0/nixcfg.git /etc/nixos
 fi
