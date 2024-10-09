887f8ae42830a5ac9983a12e02a8a6c19a24ab16
quinm0 <quin@moran1.com>
2024-10-09 16:39:31 -0400

    hmm

--- install.sh
+++ install.sh
@@ -4,15 +4,11 @@
 if [ -d "/etc/nixos/.git" ]; then
   echo "Repository already exists. Skipping cloning."
 else
-  if [ "$(ls -A /etc/nixos)" ]; then
-    echo "Directory is not empty. Performing a forced clone."
-    git clone git@github.com:quinm0/nixcfg.git /tmp/nixcfg
-    mv /tmp/nixcfg/* /etc/nixos/
-    mv /tmp/nixcfg/.* /etc/nixos/
-    rm -rf /tmp/nixcfg
-  else
-    git clone git@github.com:quinm0/nixcfg.git /etc/nixos
-  fi
+  echo "Directory is not empty. Performing a forced clone."
+  git clone git@github.com:quinm0/nixcfg.git /tmp/nixcfg
+  mv /tmp/nixcfg/* /etc/nixos/
+  mv /tmp/nixcfg/.* /etc/nixos/
+  rm -rf /tmp/nixcfg
 fi
 
 read -p "Enter the machine hostname: " hostname
